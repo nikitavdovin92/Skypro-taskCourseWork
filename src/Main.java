f135496085524173827002ee4afd6318c95b4f95
Homework3 <tradervdovin@gmail.com>
2023-02-13 20:50:45 +0500

    Решение задачи из билета по курсовой

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.util.Arrays;
+
 public class Main {
+    public static int[] removeDuplicates(int[] arr) {
+        return Arrays.stream(arr).distinct().toArray();
+    }
+
     public static void main(String[] args) {
+        int[] arr = {6, 3, 1, 4, 4, 4, 4, 5};
 
-
+        int[] distinct = removeDuplicates(arr);
+        System.out.println(Arrays.toString(distinct));
     }
 }
