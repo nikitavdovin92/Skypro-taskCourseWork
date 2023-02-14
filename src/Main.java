9c83065f6603c7e1e41203f2a03df6cb704fc680
Homework3 <tradervdovin@gmail.com>
2023-02-14 15:49:00 +0500

    Подходит любое число из диапазона int.

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        int[] arr = {6, 3, 1, 4, 4, 4, 4, 5};
+        int[] arr = {13526, 14522, 14522, 12765, 1753225, 1824355, 183333, 2543545};
 
 
         int[] distinct = removeDuplicates(arr);
